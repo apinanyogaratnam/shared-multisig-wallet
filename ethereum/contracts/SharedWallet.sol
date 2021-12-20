19f29f4c41f8c5eaeaf1ddef219024b9c11b1ca3
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-12-20 13:05:29 -0500

    added requirement statements

--- ethereum/contracts/SharedWallet.sol
+++ ethereum/contracts/SharedWallet.sol
@@ -7,23 +7,27 @@ contract SharedWallet {
 
     constructor() {
         owner = msg.sender;
-        owners.push(msg.sender);
     }
 
     modifier onlyOwner {
-        require(msg.sender == owner);
+        require(msg.sender == owner, 'only owner');
         _;
     }
 
     function addOwner(address _newOwner) external onlyOwner {
+        require(_newOwner != owner, 'cannot add contract owner to list');
+        require(_newOwner != address(0), 'cannot add null address to list');))
         owners.push(_newOwner);
     }
 
     function transferOwnership(address _newOwner) external onlyOwner {
+        require(_newOwner != owner, 'cannot transfer ownership to self');
+        require(_newOwner != address(0), 'cannot transfer ownership to null address');
         owner = _newOwner;
     }
 
     function removeOwner(uint _index) external onlyOwner {
+        require(_index < owners.length, 'index out of bounds');
         owners[_index] = owners[owners.length - 1];
         delete owners[owners.length - 1];
     }
