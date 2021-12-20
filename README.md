df4ce9c763346aab71ce857a00f2805064c1d34f
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-12-20 13:47:49 -0500

    deployed contract to rinkeby network

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Shared Wallet
 
-v1.0 contract address (rinkeby): 
+v1.0 contract address (rinkeby): 0xE09a096F16Ef4B7E9ba267bd5867fF9179a036e8
--- sharedWallet.js
+++ sharedWallet.js
@@ -3,7 +3,7 @@ import SharedWallet from './build/SharedWallet.json';
 
 const instance = new web3.eth.Contract(
     SharedWallet.abi,
-    ''
+    '0xE09a096F16Ef4B7E9ba267bd5867fF9179a036e8'
 );
 
 export default instance;
