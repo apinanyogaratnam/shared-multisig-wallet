c3d4706ec3dd1c01f9f01f0511b5c3e977f510eb
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-12-20 13:40:32 -0500

    completed contract

--- ethereum/contracts/SharedWallet.sol
+++ ethereum/contracts/SharedWallet.sol
@@ -4,6 +4,16 @@ pragma solidity ^0.8.0;
 contract SharedWallet {
     address public owner;
     address[] public owners;
+    mapping(address => bool) public isOwner;
+    uint numberOfOwners;
+
+    struct Request {
+        uint amount;
+        uint numberOfApprovals;
+        mapping(address => bool) approved;
+    }
+
+    Request[] requests;
 
     constructor() {
         owner = msg.sender;
@@ -16,8 +26,15 @@ contract SharedWallet {
 
     function addOwner(address _newOwner) external onlyOwner {
         require(_newOwner != owner, 'cannot add contract owner to list');
-        require(_newOwner != address(0), 'cannot add null address to list');))
+        require(_newOwner != address(0), 'cannot add null address to list');
+        require(!isOwner[_newOwner], 'address already exists in list');
+
+        // add to list and mark as owner
         owners.push(_newOwner);
+        isOwner[_newOwner] = true;
+
+        // increment number of owners
+        numberOfOwners++;
     }
 
     function transferOwnership(address _newOwner) external onlyOwner {
@@ -28,7 +45,48 @@ contract SharedWallet {
 
     function removeOwner(uint _index) external onlyOwner {
         require(_index < owners.length, 'index out of bounds');
+
+        // remove owner from list
         owners[_index] = owners[owners.length - 1];
         delete owners[owners.length - 1];
+        isOwner[owners[_index]] = false;
+
+        // decrement number of owners
+        numberOfOwners--;
+    }
+
+    function withdrawRequest(uint _amount) external {
+        require(_amount > 0, 'amount must be greater than 0');
+        require(isOwner[msg.sender], 'only owner can withdraw');
+
+        // create new request
+        Request storage request = requests[requests.length + 1];
+        request.amount = _amount;
+        request.numberOfApprovals = 0;
+        request.approved[msg.sender] = true;
+    }
+
+    function approve(uint _requestIndex) external {
+        require(_requestIndex < requests.length, 'index out of bounds');
+        require(requests[_requestIndex].numberOfApprovals < numberOfOwners, 'request has already been approved');
+        require(isOwner[msg.sender], 'only owner can approve');
+        require(requests[_requestIndex].approved[msg.sender] == false, 'already approved');
+
+        // increment number of approvals and mark approval
+        requests[_requestIndex].numberOfApprovals++;
+        requests[_requestIndex].approved[msg.sender] = true;
+    }
+
+    function withdraw(uint _requestIndex) external {
+        require(_requestIndex < requests.length, 'index out of bounds');
+        require(requests[_requestIndex].approved[msg.sender], 'only approved owners can withdraw');
+        require(requests[_requestIndex].numberOfApprovals == numberOfOwners, 'not all owners have approved');
+
+        // delete request
+        uint amount = requests[_requestIndex].amount;
+        delete requests[_requestIndex];
+
+        // send funds to owner
+        payable(msg.sender).transfer(amount);
     }
 }
