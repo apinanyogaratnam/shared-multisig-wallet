51513ad6bae27c93baf9dc11d831722498483fea
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-12-20 12:28:58 -0500

    changed files naming for project specific

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import example from '../ethereum/example';
+import sharedWallet from '../ethereum/sharedWallet';
 import web3 from '../ethereum/web3';
 import React from 'react';
 
