f36c299453279f9a79a8495b86e2235e39871238
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-12-20 12:31:44 -0500

    removed contract address from example contract

--- ethereum/sharedWallet.js
+++ ethereum/sharedWallet.js
@@ -3,7 +3,7 @@ import SharedWallet from './build/SharedWallet.json';
 
 const instance = new web3.eth.Contract(
     SharedWallet.abi,
-    '0x1ddcC5bC9ea017D692Eb3B9268D0c71318012d58'
+    ''
 );
 
 export default instance;
